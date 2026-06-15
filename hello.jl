61609c6e56f02da02101dabcf262b61063f535c7
yanilettemontano <yamontan@uci.edu>
2026-06-15 19:54:36 -0400

    Initial Commit

---
+++ hello.jl
@@ -0,0 +1,18 @@
+println("Hello from VS Code!")
+x = 10
+y = 20
+println(x+y)
+
+#define a function
+function greet(name)
+    println("Hello, ", name)
+end
+
+#loop 
+for i in 1:5
+    println(i)
+end
+
+#arrays
+v = [1, 2, 3]
+println(v[1]) #prints 1, does not have starting 0 index
